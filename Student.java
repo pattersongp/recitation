fff2794bee47b2f1768e37d93f08261c82197ec8
CalgoDex <goah.calvin96@gmail.com>
2017-06-16 12:28:50 -0400

    Implementing Comparable

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Student{
+public class Student implements Comparable<Student>{
 	/*
 		This class models a student with a GPA and who sleeps.
 		Written by Graham Patteson
@@ -8,16 +8,22 @@ public class Student{
 
 		private String name;
 		private double gpa;
+		private String gender;
 		private ArrayList<Integer> hours_of_sleep;
 
-	public Student(String name, double initGPA){
+	public Student(String name, double initGPA, String gender){
 		name = name;
 		gpa = initGPA;
+		this.gender = gender;
 		hours_of_sleep = new ArrayList<Integer>();
 	}
 
 	public double getGPA(){
 		return gpa;
+	}
+
+	public String getName(){
+		return name;
 	}
 
 	public void updateGPA(double update){
@@ -43,6 +49,21 @@ public class Student{
 
 		// return (double) total/counter;
 		return total/counter;
+	}
+
+	
+	@Override
+	// compareTo method for the Comparable interface
+	public int compareTo(Student stud){
+		Student doe = stud;
+		if(getGPA()<doe.getGPA()){
+			return -1;
+		}
+		if(getGPA()>doe.getGPA()){
+			return 1;
+		}
+		return 0;
+
 	}
 
 }
